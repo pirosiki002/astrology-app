7a97576e1ee3a784546b981e6c897715385e9944
SI <>
2023-09-05 17:38:17 +0900

    create top page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css'
+// import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.css'; // もしこれがある場合
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        {/* Google FontsからLobsterフォントをインポート */}
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
