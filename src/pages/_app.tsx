73cfb462624225faf6c1f4dafe33265da395e8dd
SI <>
2023-09-08 15:20:41 +0900

    ✨ responsive meta tag add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,8 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         {/* Google FontsからLobsterフォントをインポート */}
         <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
+        {/* レスポンシブデザインのためのメタタグ */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
       <Component {...pageProps} />
