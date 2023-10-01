c1543def194ccd0f2db336e681f9e38824b50c92
SI <>
2023-10-01 09:14:26 +0900

    prettier setting valid (each workspace)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,20 @@
-import Link from 'next/link';
-import styles from './index.module.css';
-import Background from '@/src/components/background';
-import TransparentBox from '../components/transparent/transparentBox';
-import Button from '../components/button';
+import Link from "next/link";
+import styles from "./index.module.css";
+import Background from "@/src/components/background";
+import TransparentBox from "../components/transparent/transparentBox";
+import Button from "../components/button";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Background />
       <TransparentBox>
-      <div className={styles.contentContainer}>
-        <h1 className={styles.title}>星占い</h1>
+        <div className={styles.contentContainer}>
+          <h1 className={styles.title}>星占い</h1>
           <Link href="/inputInfo">
-            <Button label="スタート"  />
+            <Button label="スタート" />
           </Link>
-      </div>
+        </div>
       </TransparentBox>
     </div>
   );
